d4849dd5372e0eedcdf5dc5b986afb5a08b0b987
Neil M <neilm813@gmail.com>
2021-02-02 10:29:14 -0800

    slns

--- algos/arrays/concatSelf.js
+++ algos/arrays/concatSelf.js
@@ -16,12 +16,38 @@ const expected3 = [];
 
 /**
  * Creates a new array that is a concatenation of the given array with itself.
- * - Time: O(?).
- * - Space: O(?).
+ * - Time: O(2n) -> simplified to O(n) linear. arr is looped over twice to copy
+ *    the items twice.
+ * - Space: O(2n) -> O(n) linear. The new array created has arr.length * 2.
  * @param {Array<any>} arr
  * @return {Array<any>} The resulting concatenation of the given arr with
  *    itself.
  */
+function concatArrWithSelfManually(arr) {
+  const concatArr = [];
+
+  for (let i = 0; i < 2; i++) {
+    for (const elem of arr) {
+      concatArr.push(elem);
+    }
+  }
+  return concatArr;
+}
+
 function concatArrWithSelf(arr) {
-  // code here
+  return arr.concat(arr);
+}
+
+function spreadArrWithSelf(arr) {
+  return [...arr, ...arr];
+}
+
+// causes INCORRECT order: ["a", "a", "b", "b", "c", "c"]
+function concatArrWithSelfManually2(arr) {
+  const concatArr = [];
+
+  for (const elem of arr) {
+    concatArr.push(elem, elem);
+  }
+  return concatArr;
 }
--- algos/arrays/indexOfMinVal.js
+++ algos/arrays/indexOfMinVal.js
@@ -19,11 +19,37 @@ const expected3 = -1;
 
 /**
  * Returns the index of the smallest number from the given array.
- * - Time: O(?).
- * - Space: O(?).
+ * - Time: O(n) linear, n = nums.length. Must loop through all nums to find
+ *    the minimum.
+ * - Space: O(1) constant. No new objects or arrays are created that grow in
+ *    size as the input array grows in size.
  * @param {Array<number>} nums
  * @return {number} Index of smallest number or -1 if empty given array.
  */
 function indexOfMinVal(nums) {
-  // code here
+  if (nums.length === 0) {
+    return -1;
+  }
+
+  let minIdx = 0;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] < nums[minIdx]) {
+      minIdx = i;
+    }
+  }
+  return minIdx;
+}
+
+function indexOfMinVal2(nums) {
+  let minIdx = -1;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (minIdx === -1) {
+      minIdx = i;
+    } else if (nums[i] < nums[minIdx]) {
+      minIdx = i;
+    }
+  }
+  return minIdx;
 }
