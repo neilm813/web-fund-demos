d5af2cdd4c290775a7c1675e82f079aef8553aa5
Neil M <neilm813@gmail.com>
2021-02-03 09:03:01 -0800

    algo reqs

---
+++ algos/arrays/reverseArr.js
@@ -0,0 +1,29 @@
+/* 
+  Given an array, reverse it's items in place
+  return the array after reversing it
+
+  Do it in place without using any built in methods
+*/
+
+const arr1 = [1, 2, 3];
+const expected1 = [3, 2, 1];
+
+const arr2 = ["a", "b"];
+const expected2 = ["b", "a"];
+
+const arr3 = ["a"];
+const expected3 = ["a"];
+
+const arr4 = [];
+const expected4 = [];
+
+/**
+ * Reverses the given arr in place without built in methods.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<any>} arr
+ * @return {Array<any>} The given arr after being reversed.
+ */
+function reverseArr(arr) {
+  // code here
+}
