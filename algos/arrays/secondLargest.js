3abddb2c7a900add15f0f6142fdc0116c2affe21
Neil M <neilm813@gmail.com>
2021-02-05 09:03:11 -0800

    algo reqs

---
+++ algos/arrays/secondLargest.js
@@ -0,0 +1,31 @@
+/* 
+  Array: Second-Largest
+
+  Return the second-largest element of an array, or null if there is none.
+
+  Bonus: do it with one loop and no nested loops
+*/
+
+const nums1 = [2, 3, 1, 4];
+const expected1 = 3;
+
+const nums2 = [3, 3];
+const expected2 = null;
+
+const nums3 = [2];
+const expected3 = null;
+
+const nums4 = [];
+const expected4 = null;
+
+/**
+ * Finds the second largest int from the given array and returns it or null.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums
+ * @return {?number} The second largest int from the given array or null.
+ *    The ? in front means it's nullable.
+ */
+function secondLargest(nums) {
+  // code here
+}
---
+++ algos/arrays/sumArrColumns.js
@@ -0,0 +1,18 @@
+/*
+  Input: Two arrays of equal length containing integers
+  Output: A new array where each item is the sum of
+  the items in arr1 and arr2 at that same index
+ */
+
+/**
+ * Returns a new array that is the sum of the columns of the two given arrays
+ * of equal lengths.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums1
+ * @param {Array<number>} nums2
+ * @return {Array<number>} The column sums.
+ */
+function sumArrColumns(nums1, nums2) {
+  // code here
+}
