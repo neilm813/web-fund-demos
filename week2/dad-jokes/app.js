70e72860d999330b5afcc1dff7b3982bab9eadee
Neil M <neilm813@gmail.com>
2021-03-03 15:52:55 -0800

    dad jokes pt 2

--- week2/dad-jokes/app.js
+++ week2/dad-jokes/app.js
@@ -8,16 +8,37 @@ $(document).ready(function () {
   function addJokesToDOM(data) {
     console.log(data);
 
-    var $allJokesRow = $("#row-all-jokes");
+    var $allJokesRow = $("#unrated-jokes");
     var $column = $("<div>").addClass("col p-3 shadow border");
     $allJokesRow.append($column);
 
     for (let i = 0; i < data.results.length; i++) {
       var jokeObj = data.results[i];
 
-      var $jokeParagraph = $("<p>").attr("id", jokeObj.id);
-      $jokeParagraph.text(jokeObj.joke);
-      $column.append($jokeParagraph);
+      var $jokeContainer = $("<div>")
+        .attr("id", jokeObj.id)
+        .addClass("joke-container mb-3");
+      var goodIcon = `<button class="good-icon btn btn-lg text-success">&#10003;</button>`;
+      var badIcon = `<button class="bad-icon btn btn-lg text-danger">&#10005;</button>`;
+      var jokeSpan = `<span class="h6">${jokeObj.joke}</span>`;
+      // var $jokeSpan = $("<span>").text(jokeObj.joke);
+      $jokeContainer.append(goodIcon, badIcon, jokeSpan);
+
+      $column.append($jokeContainer);
     }
   }
+
+  $(document).on("click", ".good-icon", function (event) {
+    var $jokeContainer = $(this).parent(".joke-container");
+    console.log($jokeContainer);
+
+    $("#good-jokes").append($jokeContainer);
+  });
+
+  $(document).on("click", ".bad-icon", function (event) {
+    var $jokeContainer = $(this).parent(".joke-container");
+    console.log($jokeContainer);
+
+    $("#bad-jokes").append($jokeContainer);
+  });
 });
