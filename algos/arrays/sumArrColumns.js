5cb8b39bcb76b77cf3cb203827ae47f1f503c445
Neil M <neilm813@gmail.com>
2021-03-05 14:04:14 -0800

    algos

--- algos/arrays/sumArrColumns.js
+++ algos/arrays/sumArrColumns.js
@@ -11,14 +11,20 @@ const expected1 = [30, 40, 50];
 /**
  * Returns a new array that is the sum of the columns of the two given arrays
  * of equal lengths.
- * - Time: O(?).
- * - Space: O(?).
+ * - Time: O(n) linear, n = length of either nums1 or nums2 since they will
+ *    have the same length.
+ * - Space: O(n) linear. The new array created will have the same length.
  * @param {Array<number>} nums1
  * @param {Array<number>} nums2
  * @return {Array<number>} The column sums.
  */
 function sumArrColumns(nums1, nums2) {
-  // code here
+  const summedCols = [];
+
+  for (let i = 0; i < nums1.length; i++) {
+    summedCols.push(nums1[i] + nums2[i]);
+  }
+  return summedCols;
 }
 
 // Tests
